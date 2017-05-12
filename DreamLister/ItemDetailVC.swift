4f52a518460b883f74c928ec82f1441852e8c7ee
David Santos <DSantos@MacBook-Pro-de-David.local>
2017-05-12 13:35:32 -0300

    details

---
+++ DreamLister/ItemDetailVC.swift
@@ -0,0 +1,21 @@
+//
+//  ItemDetailVC.swift
+//  DreamLister
+//
+//  Created by David Santos on 11/05/17.
+//  Copyright © 2017 dscode. All rights reserved.
+//
+
+import UIKit
+
+class ItemDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //if let topNav = self.navigationController?.navigationBar.topItem {
+        //    topNav.backBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.plain, target: nil, action: nil)
+        //}
+    }
+
+}
